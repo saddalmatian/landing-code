2586b709bbe34e2fa0dd79aad03a1614e4df834b
HuyPham <pnghuy2000@gmail.com>
2022-11-07 20:46:06 +0700

    Add Category

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,32 @@
-import { React, Component } from 'react'
-import './Navbar.css';
-import Logo from './Logo.png'
-import { Link } from 'react-router-dom';
+import { React, Component } from "react";
+import "./Navbar.css";
+import Logo from "./Logo.png";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render() {
-        return <div className='container-fluid'>
-            <div className='row navbar'>
-                <div className='col-3 navbar-element'><img className='logo-img' src={Logo} alt='Logo'></img> </div>
-                <div className='col-3 navbar-element navbar-link'><Link to='/'>Home</Link> </div>
-                <div className='col-3 navbar-element navbar-link'><Link to='/instruction'>Instruction</Link></div>
-                <div className='col-3 navbar-element navbar-link'><Link to='/category'>Category</Link></div>
-            </div>
+  render() {
+    return (
+      <div className="wrapper-navbar">
+        <div className="d-flex navbar justify-content-around">
+          <div className="navbar-element">
+            <img className="logo-img" src={Logo} alt="Logo"></img>{" "}
+          </div>
+          <div className="navbar-element navbar-link">
+            <Link to="/">Home</Link>{" "}
+          </div>
+          <div className="navbar-element navbar-link">
+            <Link to="/instruction">Instruction</Link>
+          </div>
+          <div className="navbar-element navbar-link">
+            <Link to="/category">Category</Link>
+          </div>
+          <div className="navbar-element navbar-link">
+            <Link to="/categorys">Number 1</Link>
+          </div>
         </div>
-    }
+      </div>
+    );
+  }
 }
 
 export default Navbar;
