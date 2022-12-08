b0bd1709a0bdd931d93a72e7d852e07d5e335e1c
wolfe09 <wolfe@credshare.com>
2022-12-08 20:09:54 +0700

    Change base url

--- src/requestmethod.js
+++ src/requestmethod.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://apidev.phantomal.site/dev";
+const BASE_URL = "http://thesisloadbalancer-482548701.ap-southeast-2.elb.amazonaws.com";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
